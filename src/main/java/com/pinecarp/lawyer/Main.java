d2942551f1c07fe2c2406e7b85396061aace5028
pinecarp <pinecarper@icloud.com>
2021-09-03 12:56:22 +0200

    add lock by clicking

--- src/main/java/com/pinecarp/lawyer/Main.java
+++ src/main/java/com/pinecarp/lawyer/Main.java
@@ -2,19 +2,23 @@ package com.pinecarp.lawyer;
 
 import com.pinecarp.lawyer.commands.AddLawyer;
 import com.pinecarp.lawyer.commands.Help;
-import com.pinecarp.lawyer.commands.Hunger;
+
 import com.pinecarp.lawyer.listeners.OnClick;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable(){
+
+        PluginManager pm = getServer().getPluginManager();
+
         getCommand("lawyer").setExecutor(new AddLawyer());
-        getCommand("hunger").setExecutor(new Hunger());
         getCommand("help").setExecutor(new Help());
 
-        getServer().getPluginManager().registerEvents(new OnClick(), this);
+        pm.registerEvents(new OnClick(), this);
     }
 
 
--- src/main/java/com/pinecarp/lawyer/listeners/OnClick.java
+++ src/main/java/com/pinecarp/lawyer/listeners/OnClick.java
@@ -1,13 +1,18 @@
 package com.pinecarp.lawyer.listeners;
 
+import com.pinecarp.lawyer.Main;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.Sign;
+import org.bukkit.block.TileState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 public class OnClick implements Listener {
 
@@ -20,12 +25,23 @@ public class OnClick implements Listener {
 
         if (action == Action.LEFT_CLICK_BLOCK || action == Action.RIGHT_CLICK_BLOCK) {
 
-            if(player.getInventory().getItemInMainHand().getType().equals(Material.WOODEN_PICKAXE)) {
+            if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains("Lawyer")) {
 
-                if(player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains("Lawyer")){
-                    player.sendMessage("debug");
-                }
+                player.sendMessage("Lawyer!");
+                if(!(event.getClickedBlock().getState() instanceof TileState))
+                    return;
+
+                TileState state = (TileState) event.getClickedBlock().getState();
+                PersistentDataContainer container = state.getPersistentDataContainer();
+                NamespacedKey key = new NamespacedKey(Main.getPlugin(Main.class), "private");
+
+                container.set(key, PersistentDataType.STRING, event.getPlayer().getUniqueId().toString());
+                state.update();
+                event.getPlayer().sendMessage("Locked!");
+
             }
+
+
 
         }
     }
